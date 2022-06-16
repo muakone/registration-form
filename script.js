b4797a530f1ccfc87a67d3a2f159bc5412ceac16
muak1 <muak1ayomide@gmail.com>
2022-06-16 13:39:32 +0100

    removed unused styles

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
-const claimBtn = document.getElementById("sumbitTrial");
+const sumbitTrial = document.getElementById("sumbitTrial");
 const firstName = document.getElementById("fname");
 const lastName = document.getElementById("lname");
 const email = document.getElementById("email");
 const password = document.getElementById("pwd");
-
-// error messages
 const firstError = document.querySelector("#fnameError");
 const lastError = document.querySelector("#lnameError");
 const emailError = document.querySelector("#emailError");
@@ -15,7 +13,8 @@ const errorSvg3 = document.querySelector("#errorImg3");
 const errorSvg4 = document.querySelector("#errorImg4");
 const error = document.createElement("div");
 
-// // delete error messages and
+// delete error messages and
+// change back the display
 function deleteError(error) {
   if (firstName.value !== "") {
     errorSvg1.style.display = "none";
@@ -35,58 +34,48 @@ function deleteError(error) {
   }
 }
 
-// change back the display
-function antiRed(element) {
+function removeRed(element) {
   element.style.border = "0.3px solid grey";
 }
 
-// change back to normal from error state(element,error)
-function revertNormal(element, error) {
+// remove error
+function backToNormal(element, error) {
   deleteError(error);
-  antiRed(element);
+  removeRed(element);
 }
 
-// lets add event listeners
-claimBtn.addEventListener("click", () => {
-  // form validations
+sumbitTrial.addEventListener("click", () => {
+ // // validations
 
-  //first name
+  //first name input
   if (firstName.value === "") {
-    firstName.classList.add("invalid");
     firstName.style.border = "1px solid red";
     firstError.style.display = "block";
     errorSvg1.style.display = "block";
   }
-  //last name
+  //last name input
   if (lastName.value === "") {
-    lastName.classList.add("invalid");
     lastName.style.border = "1px solid red";
     lastError.style.display = "block";
     errorSvg2.style.display = "block";
   }
-  //email
+  //email input
   if (email.value === "") {
-    email.classList.add("invalid");
     email.placeholder = "email@example/com";
     email.style.border = "1px solid red";
     emailError.style.display = "block";
     errorSvg3.style.display = "block";
   }
-  //password
+  //password input
   if (password.value === "") {
-    password.classList.add("invalid");
     password.style.border = "1px solid red";
     passwordError.style.display = "block";
     errorSvg4.style.display = "block";
   }
 });
 
-// keydown events when reentering the inputs
-firstName.addEventListener("input", () => revertNormal(firstName, firstError));
-lastName.addEventListener("input", () => revertNormal(lastName, lastError));
-email.addEventListener("input", () => revertNormal(email, emailError));
-password.addEventListener("input", () => revertNormal(password, passwordError));
+firstName.addEventListener("input", () => backToNormal(firstName, firstError));
+lastName.addEventListener("input", () => backToNormal(lastName, lastError));
+email.addEventListener("input", () => backToNormal(email, emailError));
+password.addEventListener("input", () => backToNormal(password, passwordError));
 
-
-
-//add red color to other elements
